124402fee92ca578ee66547696c5f98897e0d1b9
Harshvardhan <harsh@gmail.com>
2024-01-15 12:37:34 +0530

    Created fifth project

---
+++ PROJECT5(UNLIMITED_COLORS)/javasc.js
@@ -0,0 +1,21 @@
+const buttonStart = document.querySelector('#start');
+const buttonStop = document.querySelector('#stop');
+const bod = document.querySelector('body');
+
+
+
+let interval;
+buttonStart.addEventListener('click', function () {
+  interval = setInterval(function () {
+    let i = parseInt(Math.random() * 200 + 1);
+    let j = parseInt(Math.random() * 200 + 1);
+    let k = parseInt(Math.random() * 200 + 1);
+    bod.style.background = `rgb(${i},${j},${k})`;
+    console.log('HEkki');
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', function () {
+  clearInterval(interval);
+  interval=null;
+});
