6a9aab54a5782a179b6b4d38c7dc39b8da56422d
Harshvardhan <hjha5835@gmail.com>
2024-02-03 15:12:15 +0530

    Project 9

---
+++ PROJECT9(EMOJIS)/index.js
@@ -0,0 +1,56 @@
+const btn = document.querySelector('#emoji');
+const emojis = [
+  '😆',
+  '😅',
+  '🤣',
+  '😂',
+  '😀',
+  '🤑',
+  '🤨',
+  '🙂',
+  '😊',
+  '😗',
+  '😛',
+  '😏',
+  '🤥',
+  '😴',
+  '🥺',
+  '😧',
+  '😇',
+  '😳',
+  '🙃',
+  '🥴',
+  '🧐',
+  '🤨',
+  '😒',
+  '🤔',
+  '🤭',
+  '🥰',
+  '🤐',
+  '👀',
+  '🤔',
+  '🤪',
+  '🥲',
+  '😃',
+  '😁',
+  '😬',
+];
+
+// onmousedown: null
+// ​
+// onmousemove: null
+// ​
+// onmouseout: null
+// ​
+// onmouseover: null
+// ​
+// onmouseup: null
+
+
+
+btn.addEventListener("mouseover",()=>{
+  
+    btn.innerHTML=emojis[parseInt(Math.random()*(34))];
+  
+
+})
